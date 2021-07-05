05cbce677c32f7a082765729e2cbca2ef56f83b2
deb-ora <deh.damiani@gmail.com>
2021-07-05 15:44:48 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Button } from "./components/Button";
+import { BrowserRouter, Route } from 'react-router-dom';
+//precisa add types/react-router-dom em -d por causa do TS
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContexProvider } from './contexts/AuthContext';
+
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <AuthContexProvider>   
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </AuthContexProvider>
+    </BrowserRouter> 
   );
 }
 
 export default App;
+ 
